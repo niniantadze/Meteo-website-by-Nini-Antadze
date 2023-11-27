d6c454643ab0608bc6e4c4c01ac36de8d09fca0f
Nini Antadze <nini.antadze@gmail.com>
2023-11-27 22:09:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ function searchCity(city) {
   let apiKey = "fcec8ad3dft937afe4da000c469b9obf";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
   axios(apiUrl).then(updateWeather);
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.city;
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
 }
 let searchFormElement = document.querySelector("#search-city");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
