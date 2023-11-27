dcd0e3158717f5aceb9226908f9d63bb8c2d5a1f
Nini Antadze <nini.antadze@gmail.com>
2023-11-27 22:01:14 +0100

    added function "updateWeather" in JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-let searchFormElement = document.querySelector("#search-city");
+function updateWeather(response) {
+  let todayTemperature = document.querySelector("#today-temperature");
+  todayTemperature.innerHTML = Math.round(response.data.temperature.current);
+}
 
 function searchCity(city) {
   let apiKey = "fcec8ad3dft937afe4da000c469b9obf";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
   axios(apiUrl).then(updateWeather);
 }
-
-function updateWeather(response) {}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
@@ -15,5 +16,5 @@ function handleSearchSubmit(event) {
   h1.innerHTML = cityInput.value;
   searchCity(cityInput.value);
 }
-
+let searchFormElement = document.querySelector("#search-city");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
