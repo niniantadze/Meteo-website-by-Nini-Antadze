114454409a8f7608686aa5945f7e6d0ad4130a3f
Nini Antadze <nini.antadze@gmail.com>
2023-11-27 21:32:15 +0100

    added function "handleSearchSubmit"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 alert("hello world");
+let searchFormElement = document.querySelector("#search-city");
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = cityInput.value;
+}
+
+searchFormElement.addEventListener("submit", handleSearchSubmit);
