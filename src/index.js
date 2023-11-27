8fefc492b263dbbe859bc6fd512f6b5b83b4a2df
Nini Antadze <nini.antadze@gmail.com>
2023-11-27 21:56:55 +0100

    added funcion "searchCity" in JS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 let searchFormElement = document.querySelector("#search-city");
 
+function searchCity(city) {
+  let apiKey = "fcec8ad3dft937afe4da000c469b9obf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios(apiUrl).then(updateWeather);
+}
+
+function updateWeather(response) {}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   let h1 = document.querySelector("h1");
   h1.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
 }
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
